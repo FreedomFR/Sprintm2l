fe34c5f5b756f78d8b352c8b8ebe8b84e99fdb68
Mayol Boucaud <FreedomFR@users.noreply.github.com>
2019-10-17 16:13:20 +0200

    ajout bd.utilisateur.php

---
+++ modele/bd.utilisateur.php
@@ -0,0 +1,26 @@
+<?php
+include_once "bd.inc.php";
+
+function getUtilisateurByNom($mail, $mdp) {
+
+    try {
+        $cnx = connexionPDO();
+        $req = $cnx->prepare("select * from sallet2 where email = :mail and password=:mdp");
+        $req->bindValue(':mail', $mail, PDO::PARAM_STR);
+        $req->bindValue(':mdp', $mdp, PDO::PARAM_STR);
+        $req->execute();
+
+        $ligne = $req->fetch(PDO::FETCH_ASSOC);
+        while ($ligne) {
+            $resultat[] = $ligne;
+            $ligne = $req->fetch(PDO::FETCH_ASSOC);
+        }
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage();
+        die();
+    }
+    return $resultat;
+}
+
+
+?>
